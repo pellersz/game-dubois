8de1a241f8c36cecf2c4e3fae58dc46b04a2000e
Papp Gellert <pappgellert2003@gmail.com>
2025-08-18 14:49:56 +0300

    moved test roms to tests

--- src/main.cc
+++ src/main.cc
@@ -34,7 +34,7 @@ int main(int argc, char** argv)
     //    cpu.test(s.str());
     //}
     GameBoy gameboy(mem, cont, sc, cpu, ppu, screen); 
-    gameboy.load(std::make_shared<Cartridge>("./src/11-op a,(hl).gb"));
+    gameboy.load(std::make_shared<Cartridge>("./tests/01-special.gb"));
 
     if (argc == 1) 
         gameboy.run(false);
